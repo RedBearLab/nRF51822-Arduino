529443206d0f33ee6cda4ab4dc757fd480309dca
zoujixing <jixing.zou@redbear.hk>
2015-07-21 11:37:52 +0800

    Fix a example update error.

--- arduino-1.6.x/hardware/RBL/RBL_nRF51822/libraries/BLE_Examples/examples/BLE_Test/BLE_Test.ino
+++ arduino-1.6.x/hardware/RBL/RBL_nRF51822/libraries/BLE_Examples/examples/BLE_Test/BLE_Test.ino
@@ -1,17 +1,18 @@
 
-#include "HeartRateService.h"
-#include "DeviceInformationService.h"
-#include "ble/ServiceDiscovery.h"
-#include "ble/DiscoveredCharacteristic.h"
-#include "ble_advdata_parser.h"
-#include "ble_gap.h"
+
 
 
 BLE           ble;
-UUID service_uuid((uint16_t)0x0001);
-UUID chars_uuid1((uint16_t)0x0002);
-UUID chars_uuid2((uint16_t)0x0003);
-UUID chars_uuid3((uint16_t)0x0000);
+
+static uint8_t service1_uuid[]           = {0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static uint8_t service1_chars1[]         = {0x71, 0x3D, 0, 2, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static uint8_t service1_chars2[]         = {0x71, 0x3D, 0, 3, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static uint8_t service1_chars3[]         = {0x71, 0x3D, 0, 4, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+
+UUID service_uuid(0x180D);
+UUID chars_uuid1(0x2A37);
+UUID chars_uuid2(service1_chars2);
+UUID chars_uuid3(service1_chars3);
     
 DigitalOut    led1(P0_19);
 Serial       pc(USBTX, USBRX);
@@ -73,8 +74,14 @@ void scanCallBack(const Gap::AdvertisementCallbackParams_t *params)
     }
     else if( NRF_SUCCESS == ble_advdata_parser(BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME, params->advertisingDataLen, (uint8_t *)params->advertisingData, &len, adv_name) )
     {
-        pc.printf("Short name len : %d \r\n", len);
+        pc.printf("Long name len : %d \r\n", len);
         pc.printf("Long name is %s \r\n", adv_name);  
+        if( (len == 5) && (memcmp("HRM1", adv_name, len) == 0x00) )
+        {
+            pc.printf("Got device, stop scan \r\n");    
+            ble.stopScan();
+            ble.connect(params->peerAddr, Gap::ADDR_TYPE_RANDOM_STATIC, NULL, NULL);
+        }
     }
     pc.printf("\r\n");   
 }
@@ -82,7 +89,7 @@ void scanCallBack(const Gap::AdvertisementCallbackParams_t *params)
 void ServiceCallBack(const DiscoveredService *service)
 {
     pc.printf("Servuce Discovered............ \r\n");      
-        
+    pc.printf("UUID : %2x \r\n", service->getUUID().getShortUUID());    
 }
 
 void CharacteristicCallBack(const DiscoveredCharacteristic *chars)
@@ -95,10 +102,15 @@ void CharacteristicCallBack(const DiscoveredCharacteristic *chars)
     pc.printf("properties_notify      : %d \r\n", chars->getProperties()._notify);    
 
     pc.printf("declHandle             : %2x \r\n", chars->getDeclHandle());  
-    pc.printf("declHandle             : %2x \r\n", chars->getValueHandle());  
+    pc.printf("valueHandle             : %2x \r\n", chars->getValueHandle());  
 
-    uint16_t value = 0x0102;
-    ble.gattClient().read(conn_handle, chars->getValueHandle(), 0);
+     pc.printf("descHandle             : %2x \r\n", chars->getDescHandle());  
+    pc.printf("CCCDHandle             : %2x \r\n", chars->getCCCDHndle());     
+
+    uint16_t value = 0x0001;
+    //ble.gattClient().read(conn_handle, chars->getValueHandle(), 0);
+    //ble.gattClient().write(GattClient::GATT_OP_WRITE_CMD,conn_handle,chars->getValueHandle(),2,(uint8_t *)&value);
+    ble.gattClient().write(GattClient::GATT_OP_WRITE_REQ,conn_handle,chars->getCCCDHndle(),2,(uint8_t *)&value);
 }
 
  // GAP call back handle
@@ -116,7 +128,7 @@ static void connectionCallBack( const Gap::ConnectionCallbackParams_t *params )
     pc.printf("\r\n");  
     
     conn_handle = params->handle;
-    ble.gattClient().launchServiceDiscovery(params->handle, ServiceCallBack, NULL, service_uuid, chars_uuid3);
+    ble.gattClient().launchServiceDiscovery(params->handle, ServiceCallBack, CharacteristicCallBack, service_uuid, chars_uuid1);
 }
 
 static void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
@@ -126,7 +138,7 @@ static void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason
 
 static void discoveryTermination(Gap::Handle_t connectionHandle)
 {
-    pc.printf("discoveryTermination \r\n");    
+    pc.printf("discoveryTermination............ \r\n");    
 }
 
 static void onDataWrite(const GattWriteCallbackParams *params)
@@ -143,7 +155,7 @@ static void onDataRead(const GattReadCallbackParams *params)
     pc.printf("The data : ");
     for(uint8_t index=0; index<params->len; index++)
     {
-        pc.printf("%d ", params->data[index]);    
+        pc.printf("%X ", params->data[index]);    
     }
     pc.printf("\r\n");
 }
@@ -151,6 +163,12 @@ static void onDataRead(const GattReadCallbackParams *params)
 static void hvxCallBack(const GattHVXCallbackParams *params)
 {
     pc.printf("GattClient notify call back \r\n");  
+    pc.printf("The len : %d \r\n", params->len);
+    for(unsigned char index=0; index<params->len; index++)
+    {
+        pc.printf("%d ", params->data[index]);  
+    }
+    pc.printf("\r\n");
 }
 
 int main(void)
